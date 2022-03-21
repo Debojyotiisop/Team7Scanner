f8f3531f1517f47e57a9d17eb827727784ccec43
Lynncept <95460649+Lynncept77@users.noreply.github.com>
2022-03-21 09:14:21 +0530

    Update gbans.py

--- Team7/plugins/Mongo_DB/gbans.py
+++ Team7/plugins/Mongo_DB/gbans.py
@@ -2,7 +2,7 @@ from Team7 import MONGO_CLIENT
 from typing import Optional, Dict, Union
 from datetime import datetime
 
-db = MONGO_CLIENT["Sibyl"]["Main"]
+db = MONGO_CLIENT["Team7"]["Main"]
 
 async def get_gban(user: int) -> Optional[Dict[str, Union[str, int]]]:
     json =  await db.find_one({"user": user})
