713e38666c383a26ae574ca4e33909f963411f24
Lynnxept <98268117+Lynnxept@users.noreply.github.com>
2022-03-21 15:34:10 +0530

    Update config.py

--- Team7/config.py
+++ Team7/config.py
@@ -1,12 +1,12 @@
 from typing import Union, Optional
 
-API_ID: int = 12345
+API_ID: int = ""
 API_HASH: str = "XYZ"
 STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Team7_logs: Union[str, int] = 123456789
-Team7_Approved_logs: Union[str, int] = 123456789
+Team7_logs: Union[str, int] = ""
+Team7_Approved_logs: Union[str, int] = ""
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
-ENFORCERS: int = 123488
-INSPECTORS: int = 1993883
+ENFORCERS: int = ""
+INSPECTORS: int = ""
 MONGO_DB_URL: str = "1993838:sjsh"
