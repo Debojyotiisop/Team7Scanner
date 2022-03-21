39b53ac15d5b6067ac0b23402bb5d19ce2b26457
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-03-21 13:47:34 +0530

    Update and rename Sibyl_System/plugins/Mongo_DB/bot_settings.py to Team7/plugins/Mongo_DB/bot_settings.py

--- Team7/plugins/Mongo_DB/bot_settings.py
+++ Team7/plugins/Mongo_DB/bot_settings.py
@@ -1,7 +1,7 @@
-from Sibyl_System import MONGO_CLIENT
+from Team7 import MONGO_CLIENT
 from typing import Optional, Dict, Union
 
-db = MONGO_CLIENT["SibylSystemRobot"]["Main"]
+db = MONGO_CLIENT["Team7SystemRobot"]["Main"]
 
 
 async def get_chat(chat: int) -> Optional[Dict[str, Union[str, int]]]:
