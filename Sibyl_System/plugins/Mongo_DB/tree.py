f56b1bee5a19d71b2207cca69cc61cfa478ba8ff
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-03-21 13:50:01 +0530

    Update tree.py
    Changed Directory

--- Sibyl_System/plugins/Mongo_DB/tree.py
+++ Sibyl_System/plugins/Mongo_DB/tree.py
@@ -1,8 +1,8 @@
-from Sibyl_System import MONGO_CLIENT
+from Team7 import MONGO_CLIENT
 from datetime import datetime
 from random import choice
 
-db = MONGO_CLIENT["Sibyl"]["Main"]
+db = MONGO_CLIENT["Team7"]["Main"]
 
 
 async def get_data() -> dict:
@@ -10,11 +10,11 @@ async def get_data() -> dict:
     return data
 
 
-async def add_inspector(sibyl: int, inspector: int) -> True:
+async def add_inspector(team7: int, inspector: int) -> True:
     data = await get_data()
-    data["data"][str(sibyl)][str(inspector)] = []
+    data["data"][str(team7)][str(inspector)] = []
     data["standalone"][str(inspector)] = {
-        "addedby": sibyl,
+        "addedby": team7,
         "timestamp": datetime.timestamp(datetime.now()),
     }
     await db.replace_one(await get_data(), data)
@@ -22,12 +22,12 @@ async def add_inspector(sibyl: int, inspector: int) -> True:
 
 async def add_enforcers(inspector: int, enforcer: int) -> True:
     data = await get_data()
-    sibyl = data["standalone"][str(inspector)]["addedby"]
+    team7 = data["standalone"][str(inspector)]["addedby"]
     if sibyl == 777000:
         s = data["data"][str(inspector)]
         s[list(choice(s.keys()))].append([enforcer])
     else:
-        data["data"][str(sibyl)][str(inspector)].append([enforcer])
+        data["data"][str(team7)][str(inspector)].append([enforcer])
     data["standalone"][str(enforcer)] = {
         "addedby": inspector,
         "timestamp": datetime.timestamp(datetime.now()),
