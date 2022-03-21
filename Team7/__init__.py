60b159d2f4f7248a2bcf7e0762de75b1ee7a9f9e
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-03-21 13:13:12 +0530

    Update and rename Sibyl_System/__init__.py to Team7/__init__.py
    changed sibyl to Team7

--- Team7/__init__.py
+++ Team7/__init__.py
@@ -26,49 +26,49 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    RAW_SIBYL = os.environ.get("SIBYL", "")
+    RAW_TEAM7 = os.environ.get("TEAM7", "")
     RAW_ENFORCERS = os.environ.get("ENFORCERS", "")
-    SIBYL = list(int(x) for x in os.environ.get("SIBYL", "").split())
+    TEAM7 = list(int(x) for x in os.environ.get("TEAM7", "").split())
     INSPECTORS = list(int(x) for x in os.environ.get("INSPECTORS", "").split())
     ENFORCERS = list(int(x) for x in os.environ.get("ENFORCERS", "").split())
     MONGO_DB_URL = os.environ.get("MONGO_DB_URL")
-    Sibyl_logs = int(os.environ.get("Sibyl_logs"))
-    Sibyl_approved_logs = int(os.environ.get("Sibyl_Approved_Logs"))
+    Team7_logs = int(os.environ.get("Team7_logs"))
+    Team7_approved_logs = int(os.environ.get("Team7_Approved_Logs"))
     GBAN_MSG_LOGS = int(os.environ.get("GBAN_MSG_LOGS"))
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
 else:
-    import Sibyl_System.config as Config
+    import Team7.config as Config
 
     API_ID_KEY = Config.API_ID
     API_HASH_KEY = Config.API_HASH
     STRING_SESSION = Config.STRING_SESSION
     MONGO_DB_URL = Config.MONGO_DB_URL
-    with open(os.path.join(os.getcwd(), "Sibyl_System/elevated_users.json"), "r") as f:
+    with open(os.path.join(os.getcwd(), "Team7/elevated_users.json"), "r") as f:
         data = json.load(f)
-    SIBYL = data["SIBYL"]
+    SIBYL = data["TEAM7"]
     ENFORCERS = data["ENFORCERS"]
     INSPECTORS = data["INSPECTORS"]
-    Sibyl_logs = Config.Sibyl_logs
-    Sibyl_approved_logs = Config.Sibyl_approved_logs
+    Team7_logs = Config.Team7_logs
+    Team7_approved_logs = Config.Team7_approved_logs
     GBAN_MSG_LOGS = Config.GBAN_MSG_LOGS
     BOT_TOKEN = Config.BOT_TOKEN
 
-INSPECTORS.extend(SIBYL)
+INSPECTORS.extend(TEAM7)
 ENFORCERS.extend(INSPECTORS)
 
 session = aiohttp.ClientSession()
 
 MONGO_CLIENT = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
 
-from .client_class import SibylClient
+from .client_class import Team7Client
 
 try:
-    System = SibylClient(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
+    System = Team7Client(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
 except:
     print(traceback.format_exc())
     exit(1)
 
-collection = MONGO_CLIENT["Sibyl"]["Main"]
+collection = MONGO_CLIENT["Team7"]["Main"]
 
 
 async def make_collections() -> str:
@@ -118,12 +118,12 @@ def system_cmd(
         args["pattern"] = re.compile(r"[\?\.!/](" + pattern + r")(?!@)")
     else:
         args["pattern"] = re.compile(r"[\?\.!]" + pattern)
-    if allow_sibyl and allow_enforcer:
+    if allow_team7 and allow_enforcer:
         args["from_users"] = ENFORCERS
-    elif allow_inspectors and allow_sibyl:
+    elif allow_inspectors and allow_team7:
         args["from_users"] = INSPECTORS
     else:
-        args["from_users"] = SIBYL
+        args["from_users"] = TEAM7
     if force_reply:
         args["func"] = lambda e: e.is_reply
     return events.NewMessage(**args)
