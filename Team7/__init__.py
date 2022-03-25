78ac989b2393243a56dd0c2c5f127dc251e861dc
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-03-25 00:50:39 -0700

    hm

--- Team7/__init__.py
+++ Team7/__init__.py
@@ -1,5 +1,6 @@
 """Gets ENV vars or Config vars then calls class."""
 
+
 from telethon import events
 from telethon.sessions import StringSession
 from motor import motor_asyncio
@@ -12,6 +13,8 @@ import logging
 import os
 import re
 
+if os.path.exists('log.txt'):
+    os.remove("log.txt")
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -26,76 +29,78 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    RAW_TEAM7 = os.environ.get("TEAM7", "")
+    RAW_SIBYL = os.environ.get("SIBYL", "")
     RAW_ENFORCERS = os.environ.get("ENFORCERS", "")
-    TEAM7 = list(int(x) for x in os.environ.get("TEAM7", "").split())
-    INSPECTORS = list(int(x) for x in os.environ.get("INSPECTORS", "").split())
-    ENFORCERS = list(int(x) for x in os.environ.get("ENFORCERS", "").split())
+    SIBYL = [int(x) for x in os.environ.get("SIBYL", "").split()]
+    INSPECTORS = [int(x) for x in os.environ.get("INSPECTORS", "").split()]
+    ENFORCERS = [int(x) for x in os.environ.get("ENFORCERS", "").split()]
     MONGO_DB_URL = os.environ.get("MONGO_DB_URL")
-    Team7_logs = int(os.environ.get("Team7_logs"))
-    Team7_Approved_logs = int(os.environ.get("Team7_Approved_logs"))
-    GBAN_MSG_LOGS = int(os.environ.get("GBAN_MSG_LOGS"))
+    Sibyl_logs = int(os.environ.get("Sibyl_logs"))
+    Sibyl_approved_logs = int(os.environ.get("Sibyl_Approved_Logs"))
+    GBAN_MSG_LOGS = [int(x) for x in os.environ.get("GBAN_MSG_LOGS", "").split()]
+    GBAN_MSG_LOGS1 = "-1001580719176"
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
 else:
-    import Team7.config as Config
+    import Sibyl_System.config as Config
 
     API_ID_KEY = Config.API_ID
     API_HASH_KEY = Config.API_HASH
     STRING_SESSION = Config.STRING_SESSION
     MONGO_DB_URL = Config.MONGO_DB_URL
-    with open(os.path.join(os.getcwd(), "Team7/elevated_users.json"), "r") as f:
+    with open(os.path.join(os.getcwd(), "Sibyl_System/elevated_users.json"), "r") as f:
         data = json.load(f)
-    TEAM7 = data["TEAM7"]
+    SIBYL = data["SIBYL"]
     ENFORCERS = data["ENFORCERS"]
     INSPECTORS = data["INSPECTORS"]
-    Team7_logs = Config.Team7_logs
-    Team7_Approved_logs = Config.Team7_Approved_logs
+    Sibyl_logs = Config.Sibyl_logs
+    Sibyl_approved_logs = Config.Sibyl_approved_logs
     GBAN_MSG_LOGS = Config.GBAN_MSG_LOGS
     BOT_TOKEN = Config.BOT_TOKEN
 
-INSPECTORS.extend(TEAM7)
+INSPECTORS.extend(SIBYL)
 ENFORCERS.extend(INSPECTORS)
 
 session = aiohttp.ClientSession()
 
 MONGO_CLIENT = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
 
-from .client_class import Team7Client
+from .client_class import SibylClient
 
-#try:
-System = Team7Client(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
-#except:
-#    print(traceback.format_exc())
- #   exit(1)
+try:
+    System = SibylClient(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
+except:
+    print(traceback.format_exc())
+    exit(1)
 
-collection = MONGO_CLIENT["Team7"]["Main"]
+collection = MONGO_CLIENT["Sibyl"]["Main"]
 
 
 async def make_collections() -> str:
     if (
         await collection.count_documents({"_id": 1}, limit=1) == 0
     ):  # Blacklisted words list
-        dictw = {"_id": 1}
-        dictw["blacklisted"] = []
+        dictw = {'_id': 1, 'blacklisted': []}
         await collection.insert_one(dictw)
 
     if (
         await collection.count_documents({"_id": 2}, limit=1) == 0
     ):  # Blacklisted words in name list
-        dictw = {"_id": 2, "Type": "Wlc Blacklist"}
-        dictw["blacklisted_wlc"] = []
+        dictw = {'_id': 2, 'Type': 'Wlc Blacklist', 'blacklisted_wlc': []}
         await collection.insert_one(dictw)
     if await collection.count_documents({"_id": 3}, limit=1) == 0:  # Gbanned users list
-        dictw = {"_id": 3, "Type": "Gban:List"}
-        dictw["victim"] = []
-        dictw["gbanners"] = []
-        dictw["reason"] = []
-        dictw["proof_id"] = []
+        dictw = {
+            '_id': 3,
+            'Type': 'Gban:List',
+            'victim': [],
+            'gbanners': [],
+            'reason': [],
+            'proof_id': [],
+        }
+
         await collection.insert_one(dictw)
     if await collection.count_documents({"_id": 4}, limit=1) == 0:  # Rank tree list
-        sample_dict = {"_id": 4, "data": {}, "standalone": {}}
-        sample_dict["data"] = {}
-        for x in TEAM7:
+        sample_dict = {'_id': 4, 'standalone': {}, 'data': {}}
+        for x in SIBYL:
             sample_dict["data"][str(x)] = {}
             sample_dict["standalone"][str(x)] = {
                 "added_by": 777000,
@@ -107,7 +112,7 @@ async def make_collections() -> str:
 
 def system_cmd(
     pattern=None,
-    allow_TEAM7=True,
+    allow_sibyl=True,
     allow_enforcer=False,
     allow_inspectors=False,
     allow_slash=True,
@@ -118,12 +123,12 @@ def system_cmd(
         args["pattern"] = re.compile(r"[\?\.!/](" + pattern + r")(?!@)")
     else:
         args["pattern"] = re.compile(r"[\?\.!]" + pattern)
-    if allow_TEAM7 and allow_enforcer:
+    if allow_sibyl and allow_enforcer:
         args["from_users"] = ENFORCERS
-    elif allow_inspectors and allow_TEAM7:
+    elif allow_inspectors and allow_sibyl:
         args["from_users"] = INSPECTORS
     else:
-        args["from_users"] = TEAM7
+        args["from_users"] = SIBYL
     if force_reply:
         args["func"] = lambda e: e.is_reply
     return events.NewMessage(**args)
