5a075a85d3d6440f209fb41d3d1df63b4d156d6c
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-03-25 01:17:29 -0700

    Update config.py

--- Team7/config.py
+++ Team7/config.py
@@ -3,7 +3,7 @@ from typing import Union, Optional
 API_ID: int = ""
 API_HASH: str = "XYZ"
 STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Team7_logs: Union[str, int] = ""
+Sibyl_logs: Union[str, int] = ""
 Team7_Approved_logs: Union[str, int] = ""
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
