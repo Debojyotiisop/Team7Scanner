72e44ba281db430d5ea28ef7b4aed3d5ea65ef81
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-03-25 00:58:26 -0700

    done

--- Team7/__main__.py
+++ Team7/__main__.py
@@ -4,14 +4,18 @@ from Team7 import (
     make_collections,
     INSPECTORS,
     ENFORCERS,
-    Team7_logs,
-    TEAM7,
+    Sibyl_logs,
 )
-from Team7.strings import on_string
+from telethon import TelegramClient, events, Button, types, functions, errors
+from Sibyl_System.strings import on_string
 import logging
 import importlib
 import asyncio
 import time
+from Team7 import SIBYL
+from sys import version as ver
+from telethon import version as vers
+
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
@@ -29,7 +33,7 @@ for load in to_load:
         if not hasattr(imported, "__plugin_name__"):
             imported.__plugin_name__ = imported.__name__
 
-        if not imported.__plugin_name__.lower() in IMPORTED:
+        if imported.__plugin_name__.lower() not in IMPORTED:
             IMPORTED[imported.__plugin_name__.lower()] = imported
 
         if hasattr(imported, "help_plus") and imported.help_plus:
@@ -42,26 +46,43 @@ for load in to_load:
         print("------------------------------------")
 
 
-@System.on(system_cmd(pattern=r"t7info", allow_enforcer=True,allow_inspectors=True))
+        
+@System.on(system_cmd(pattern="t7stats"))
+async def stats(event):
+    msg = f"Processed {System.processed} messages since last restart."
+    msg += f"\n{len(ENFORCERS)} Enforcers & {len(INSPECTORS)} Inspectors"
+    g = 0
+    async for d in event.client.iter_dialogs(limit=None):
+        if d.is_channel and not d.entity.broadcast:
+            g += 1
+        elif d.is_group:
+            g += 1
+    msg += f"\nModerating {g} Groups"
+    await event.reply(msg)
+    
+
+@System.on(system_cmd(pattern=r"info", allow_enforcer=True))
 async def status(event):
-    msg = await event.reply("Conecting to Slayer System.")
+    msg = await event.reply("Portable Psychological Diagnosis and Suppression System.")
     time.sleep(1)
-    await msg.edit("Initialising ■□□□□□")
+    await msg.edit("Initialising ▫️◾️◾️")
     time.sleep(1)
-    await msg.edit("Initialising ■■□□□□")
+    await msg.edit("Initialising ▫️▫️◾️")
     time.sleep(1)
-    await msg.edit("Initialising ■■■□□□")
+    await msg.edit("Initialising ◾️◾️▫️")
     time.sleep(1)
-    await msg.edit("Initialising ■■■■□□")
+    await msg.edit("Initialising ◾️▫️▫️")
     time.sleep(1)
-    await msg.edit("Initialising ■■■■■□")
+    await msg.edit("Initialising ▫️▫️▫️")
     time.sleep(1)
-    await msg.edit("Initialising ■■■■■■")
+    await msg.edit("Connection successful!")
     time.sleep(1)
+    await msg.edit("You are an Verified User")
+    time.sleep(2)
     await msg.edit("🔰🔰VERIFIED🔰🔰")
     time.sleep(2)
     senderx = await event.get_sender()
-    if event.sender.id in TEAM7:
+    if event.sender.id in SIBYL:
         user_status = "**Developer**"
     elif event.sender.id in INSPECTORS:
         user_status = "**Inspector**"
@@ -70,9 +91,46 @@ async def status(event):
     time.sleep(1)
     await msg.edit(on_string.format(Enforcer=user_status, name=senderx.first_name))
 
+    
+    
+@System.on(system_cmd(pattern=r"status", allow_enforcer=True))
+async def status(event):
+    msg = await event.reply("Hold on have Give me a second Featching the Node Status.")
+    time.sleep(1)
+    await msg.edit("Processing ■□□□□□")
+    time.sleep(1)
+    await msg.edit("Processing ■■□□□□")
+    time.sleep(1)
+    await msg.edit("Processing ■■■□□□")
+    time.sleep(1)
+    await msg.edit("Processing ■■■■□□")
+    time.sleep(1)
+    await msg.edit("Processing ■■■■■□")
+    time.sleep(1)
+    await msg.edit("Current Node Status")
+    time.sleep(1)
+    await msg.edit("*Version = v16.9.1\n\n*Codename = Gallium\n\n*Status = LTS\n\n*Total Core = 2")
+    time.sleep(2)
+    sender = await event.get_sender()
+    user_status = "Inspector" if sender.id in INSPECTORS else "Enforcer"
+    time.sleep(1)
+    await msg.edit("¤ Working on Node 13.7\n\n¤ Python Version {ver}\n\n¤Telegram Version {vers}.")    
 
 
-@System.on(system_cmd(pattern="t7stats"))
+
+@System.on(system_cmd(pattern=r"axel", allow_slash=False, allow_inspectors=True))
+async def send_help(event):
+    from_ = await System.get_entity(event.sender_id)
+    await event.reply(
+        start_msg.format(user=from_.first_name),
+        buttons=start_buttons,
+        link_preview=False,
+    )
+    
+    
+    
+    
+@System.on(system_cmd(pattern="stats"))
 async def stats(event):
     msg = f"Processed {System.processed} messages since last restart."
     msg += f"\n{len(ENFORCERS)} Enforcers & {len(INSPECTORS)} Inspectors"
@@ -86,13 +144,13 @@ async def stats(event):
     await event.reply(msg)
 
 
-@System.on(system_cmd(pattern=r"help", allow_slash=False, allow_inspectors=True))
+@System.on(system_cmd(pattern=r"help", allow_slash=False, allow_inspectors=True, allow_enforcer=True))
 async def send_help(event):
     try:
         help_for = event.text.split(" ", 1)[1].lower()
     except IndexError:
         msg = "List of plugins with help text:\n"
-        for x in HELP:
+        for x in HELP.keys():
             msg += f"`{x.capitalize()}`\n"
         await event.reply(msg)
         return
@@ -115,10 +173,11 @@ async def main():
         msg = "Few plugins failed to load:"
         for plugin in FAILED_TO_LOAD:
             msg += f"\n**{plugin}**\n\n`{FAILED_TO_LOAD[plugin]}`"
-        await System.send_message(Team7_logs, msg)
+        await System.send_message(Sibyl_logs, msg)
     else:
-        await System.send_message(Team7_logs, "I'm up!")
+        await System.send_message(Sibyl_logs, "I am up!!")
     await System.run_until_disconnected()
+
 
 
 if __name__ == "__main__":
