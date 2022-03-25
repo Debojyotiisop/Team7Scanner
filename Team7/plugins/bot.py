89e8944db84448b81129ad1b999c073b9eb3d0d2
Lynnxept <98268117+Lynnxept@users.noreply.github.com>
2022-03-25 15:44:28 +0530

    Update bot.py

--- Team7/plugins/bot.py
+++ Team7/plugins/bot.py
@@ -70,7 +70,7 @@ async def make_proof(user: Union[str, int]):
 @System.bot.on(events.NewMessage(pattern="[/!]start"))
 async def start(_, message):
         if message.chat.type != "private":
-        return await message.reply(
+        await message.reply(
             "Pm Me For More Details.", reply_markup=keyboard
         )
 else:
